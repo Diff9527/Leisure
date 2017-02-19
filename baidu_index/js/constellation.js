a6b4a220aeccb3c6c872234aab4bb81399b54374
Diff <562732264@qq.com>
2017-02-19 14:13:46 +0800

    百度首页

--- baidu_index/js/constellation.js
+++
@@ -1,8 +0,0 @@
-//点击星座
-define(['jquery'], function () {
-    $(function () {
-        $('.mine-title').click(function () {
-            $('.s-block-container .s-mblock-content').slideToggle();
-        });
-    });
-});
--- baidu_index/js/custom.js
+++
@@ -1,8 +0,0 @@
-//点击自定义
-define(['jquery'], function (jquery) {
-    $(function () {
-        $('.tips-manager-btn').click(function () {
-            $('.sui-dialog-minesetting').slideToggle();
-        });
-    });
-});
